278ce28781e94e357726f736d3af49d1bb758262
damien <rifflartdamien68@gmail.com>
2025-01-31 15:32:24 +0100

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from 'react';
-import { Button } from "@/components/ui/button"
+import { Button } from "../components/ui/button"
 
 export default function Home() {
   const noms = ['Thibault', 'Eliott', 'Elsa', 'Damien', 'Bastien'];
