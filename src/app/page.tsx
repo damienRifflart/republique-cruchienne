a9a5c16b32b68350e3b5f2ffeebde706f1294331
Dams <rifflartdamien68@gmail.com>
2025-02-07 20:01:36 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import { useState } from 'react';
 export default function Home() {
   const [isAnimating, setIsAnimating] = useState(false); // animation
 
-  const [noms, setNoms] = useState(['Thibauld', 'Eliott', 'Elsa', 'Damien', 'Bastien']);
+  const [noms, setNoms] = useState(['Thibaud', 'Eliott', 'Elsa', 'Damien', 'Bastien']);
   const [couleurs, setCouleurs] = useState(['#554971', '#8AC6D0', '#FF6663', '#DDF45B', '#CBE896']);
   const [nouveauNom, setNouveauNom] = useState('');
 
-  const [nomChoisi, setNomChoisi] = useState('Thibault');
-  const [couleurChoisi, setCouleurChoisi] = useState('red');
+  const [nomChoisi, setNomChoisi] = useState('Thibaud');
+  const [couleurChoisi, setCouleurChoisi] = useState('#554971');
 
   const choisirNouveauNom = () => {
     const random_n = Math.floor(Math.random() * noms.length)
@@ -23,7 +23,7 @@ export default function Home() {
   };
 
   const ajouterNom = () => {
-    const random_color = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`
+    let random_color = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`
 
     setNoms([...noms, nouveauNom]);
     setCouleurs([...couleurs, random_color])
@@ -31,7 +31,7 @@ export default function Home() {
   } 
 
   return (
-      <div className={`flex justify-center items-center h-screen w-screen bg-[${couleurChoisi}]`} onClick={choisirNouveauNom}>
+      <div className={`flex justify-center items-center h-screen w-screen`} style={{backgroundColor: couleurChoisi }} onClick={choisirNouveauNom}>
         <h1 className={`text-5xl transition-all duration-300 ${isAnimating ? "scale-125" : ""}`}>{nomChoisi}</h1>
         <div className="w-[20rem] h-[20rem] bg-white rounded-xl absolute right-20" onClick={(e) => e.stopPropagation()}>
           <div className="flex items-center mt-5 w-full flex-col">
